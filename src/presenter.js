4d26a10520165575da7864e3d144c9d70b5775b1
Josue-Camacho <josuecamacho582@gmail.com>
2026-03-02 21:58:33 -0400

    feat(ui): agregar interfaz con botones para marcar puntos

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,21 @@
-import sumar from "./sumador";
+import Tennis from "./tennis.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const tennis = new Tennis();
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+const scoreElement = document.querySelector("#score");
+const player1Btn = document.querySelector("#player1-btn");
+const player2Btn = document.querySelector("#player2-btn");
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+function updateScore() {
+  scoreElement.innerText = tennis.score();
+}
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+player1Btn.addEventListener("click", () => {
+  tennis.player1Scores();
+  updateScore();
+});
+
+player2Btn.addEventListener("click", () => {
+  tennis.player2Scores();
+  updateScore();
 });
