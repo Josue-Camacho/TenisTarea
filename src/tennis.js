0ad441de431878acafef74ef27d46f40951b411f
Josue-Camacho <josuecamacho582@gmail.com>
2026-03-02 21:17:07 -0400

    feat:creando el repositorio

---
+++ src/tennis.js
@@ -0,0 +1,24 @@
+class Tennis {
+    constructor() {
+        this.player1Points = 0;
+    }
+    score(){
+        let player1Score = "Love";
+        if(this.player1Points == 1){
+            player1Score= "15"
+        }
+        if(this.player1Points == 2){
+            player1Score= "30"
+        }
+        if(this.player1Points == 3){
+            player1Score= "40"
+        }
+        return player1Score+" - Love"
+    }
+
+    player1Scores(){
+        this.player1Points++;
+    }
+}
+
+export default Tennis;
